c12bd79b0f2c0d7c0d8759605615b8499b3ec87c
vahidiranshad <VahidIranshad>
2022-05-14 00:57:56 +0430

    Add Configuration

---
+++ src/DomainLayer/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<DomainLayer.Persistence.WPFNTierHahnDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DomainLayer.Persistence.WPFNTierHahnDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
