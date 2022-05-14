71dec7b8697b9db3be69912338ffa56ea3a075a9
VahidIranshad <103821484+VahidIranshad@users.noreply.github.com>
2022-05-14 18:19:38 +0430

    View

---
+++ src/PresentationLayer/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using PresentationLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
